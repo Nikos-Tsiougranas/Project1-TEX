2e9b6f8c3bd8ec04f98a4ecbdeae35a1b0351a5a
Nikos-Tsiougranas <nikosp1339@gmail.com>
2018-04-29 07:36:25 +0300

    Add files via upload

--- Text_preprocess/text_processing.py
+++ Text_preprocess/text_processing.py
@@ -3,23 +3,29 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
+import pandas as pd
+import numpy as np
 lmtzr = WordNetLemmatizer()
 ps = PorterStemmer()
 
 def stemming(train_data):
+    train_data=train_data.values.tolist()    
     for i in range(0,len(train_data)):
         train_data[i][3]= re.sub(r'[^\x00-\x7F]',' ', train_data[i][3])
         train_data[i][3]= [ps.stem(word) for word in train_data[i][3].split( )]
         train_data[i][3]= ' '.join(train_data[i][3])
         train_data[i][3]=train_data[i][3].encode('ascii')
+    train_data = pd.DataFrame(np.array(train_data).reshape(-1,5), columns = ['RowNum','Id','Title','Content','Category'])
     return train_data
 
 def lemmatization(train_data):
+    train_data=train_data.values.tolist()    
     for i in range(0,len(train_data)):
         train_data[i][3]= re.sub(r'[^\x00-\x7F]',' ', train_data[i][3])
         train_data[i][3]= [lmtzr.lemmatize(word) for word in train_data[i][3].split( )]
         train_data[i][3]= ' '.join(train_data[i][3])
         train_data[i][3]=train_data[i][3].encode('ascii')
+    train_data = pd.DataFrame(np.array(train_data).reshape(-1,5), columns = ['RowNum','Id','Title','Content','Category'])
     return train_data
     
 def lemstopwords():
